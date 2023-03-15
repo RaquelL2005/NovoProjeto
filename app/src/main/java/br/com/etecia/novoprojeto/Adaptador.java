059f4c7d2fa956b9d77d4e70a3b703da50a03179
Raquell2005 <raqdezotti@gmail.com>
2023-03-14 22:13:48 -0300

    chamando o adaptador

--- app/src/main/java/br/com/etecia/novoprojeto/Adaptador.java
+++ app/src/main/java/br/com/etecia/novoprojeto/Adaptador.java
@@ -1,12 +1,28 @@
 package br.com.etecia.novoprojeto;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolder> {
+
+    private Context mContexto;
+    private List<Filmes> lstFilmes;
+
+    public Adaptador(Context mContexto, List<Filmes> lstFilmes) {
+        this.mContexto = mContexto;
+        this.lstFilmes = lstFilmes;
+    }
+
+    //criando o construtor da classe com parâmetros
 
     @NonNull
     @Override
@@ -25,10 +41,20 @@ public class Adaptador extends RecyclerView.Adapter<Adaptador.ViewHolder> {
     }
 
     //criar a classe ViewHolder
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView txtTitulo;
+        ImageView imagemFilmes;
+        CardView cardFilmes;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            txtTitulo = itemView.findViewById(R.id.idTituloFilmes);
+            imagemFilmes = itemView.findViewById(R.id.idImgFilmes);
+            cardFilmes = itemView.findViewById(R.id.idCardViewFilmes);
+
         }
     }
 
--- app/src/main/java/br/com/etecia/novoprojeto/MainActivity.java
+++ app/src/main/java/br/com/etecia/novoprojeto/MainActivity.java
@@ -4,11 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    List<Filmes> listaFilmes;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //criando a base de dados para carregamento da lista de filmes
+        listaFilmes = new ArrayList<>();
+
+        listaFilmes.add(
+                 new Filmes("Casamento Armado","Darcy (Jennifer Lopez) and Tom (Josh Duhamel) gather their families for the ultimate destination wedding but when the entire party is taken hostage",
+                         "Comédia",
+                         R.drawable.casamento)
+        );
+
+
+        //instanciando a classe do adaptador e passando os parâmetros
+        Adaptador adaptador = new Adaptador(getApplicationContext(), listaFilmes);
     }
+
 }
